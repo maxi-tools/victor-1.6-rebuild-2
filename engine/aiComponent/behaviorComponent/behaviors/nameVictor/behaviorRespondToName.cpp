351a9ceaefc438ecfd1fd5f1aa339e36650b643d
Switch-modder <deolrajjyot@gmail.com>
2026-04-12 22:51:33 -0400

    FUUU

--- engine/aiComponent/behaviorComponent/behaviors/nameVictor/behaviorRespondToName.cpp
+++ engine/aiComponent/behaviorComponent/behaviors/nameVictor/behaviorRespondToName.cpp
@@ -98,7 +98,7 @@ void BehaviorRespondToName::OnBehaviorActivated()
     _name = Util::FileUtils::ReadFile("/data/data/rebuild/customBotName");
   } else if (Util::FileUtils::FileExists("/data/data/customBotName")) {
     Util::FileUtils::MoveFile("/data/data/rebuild/customBotName", "/data/data/customBotName");
-    _name = Util::FileUtils::ReadFile("/data/data/customBotName");
+    _name = Util::FileUtils::ReadFile("/data/data/rebuild/customBotName");
   } else {
     _name = "Vector";
   }
